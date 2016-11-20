c17b9a91cd88e017f5e7ac300884de0777e7aaba
Isaac López <izaak00@hotmail.com>
2016-11-20 01:04:19 -0600

    expreciones regulares

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,7 @@
 import urllib
+import re
 
+#primera forma 
 def get_page(file_path):
 	open_file = open(file_path,'w')
 	html_file = urllib.urlopen('http://econpy.pythonanywhere.com/ex/001.html')
@@ -21,7 +23,17 @@ def get_tittle(file_path):
 			final_post = sentence.find(regex_end)
 			print sentence[initial_post:final_post]
 
+#titulos con expreciones regulares
+def get_tittle_regex():
+	html_file = urllib.urlopen('http://econpy.pythonanywhere.com/ex/001.html')
+	html_file = html_file.read()
+	regex = '<div title="buyer-name">(.+?)</div>'
+	titles = re.findall(regex,html_file)
+	for title in titles:
+		print title
+
 if __name__ == '__main__':
 	file_path = 'econpy.html'
-	get_page(file_path)
-	get_tittle(file_path)
+	#get_page(file_path)
+	#get_tittle(file_path)
+	get_tittle_regex()
